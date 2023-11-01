65545f5596b23d37618271603874eef6cf397678
Ib soro <soro4827@gmail.com>
2023-11-01 23:47:25 +0000

    fix: hooks

--- apps/clnapp/admin.py
+++ apps/clnapp/admin.py
@@ -2,16 +2,20 @@ from django.contrib import admin
 from clnapp.models import Badge, Model3d, UserBadge
 # Register your models here.
 
-admin.site.register(Badge)
+
 class BadgeAdmin(admin.ModelAdmin):
     list_display = ("name", "logo")
     
 
-admin.site.register(UserBadge)
+
 class UserBadgeAdmin(admin.ModelAdmin):
     pass
 
-admin.site.register(Model3d)
+
 class Model3dAdmin(admin.ModelAdmin):
     list_display = ("title", "author", "nb_views", "created_at", "updated_at")
-    date_hierarchy = ("created_at",)
+
+
+admin.site.register(Badge, BadgeAdmin)
+admin.site.register(UserBadge, UserBadgeAdmin)
+admin.site.register(Model3d, Model3dAdmin)
--- apps/clnapp/models/__init__.py
+++ apps/clnapp/models/__init__.py
@@ -1,2 +1,5 @@
 from .badge import Badge, UserBadge
 from .m3d import Model3d
+
+
+from .hooks import __name__ as hooks
