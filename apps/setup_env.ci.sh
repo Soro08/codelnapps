2d46f61f72d0ffd3f46c921ed0ff300fc37b9221
Ib soro <soro4827@gmail.com>
2023-11-03 23:38:34 +0000

    test

--- apps/setup_env.ci.sh
+++ apps/setup_env.ci.sh
@@ -4,3 +4,9 @@ echo SECRET_KEY=$SECRET_KEY >> ./.env
 echo DJANGO_ALLOWED_HOSTS="localhost 127.0.0.1 [::1]" >> ./.env
 echo CELERY_BROKER=$CELERY_BROKER >> ./.env
 echo CELERY_BACKEND=$CELERY_BACKEND >> ./.env
+
+
+echo WEB_IMAGE=$WEB_IMAGE >> ./.env
+echo CELERY_IMAGE=$CELERY_IMAGE >> ./.env
+echo BEAT_IMAGE=$BEAT_IMAGE >> ./.env
+echo DASH_IMAGE=$DASH_IMAGE >> ./.env
