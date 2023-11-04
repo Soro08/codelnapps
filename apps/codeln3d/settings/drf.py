0ac37e18b13e1dd80814582e2dad653350983bec
Ib soro <soro4827@gmail.com>
2023-11-04 19:49:17 +0000

    fix: update docker

--- apps/codeln3d/settings/drf.py
+++ apps/codeln3d/settings/drf.py
@@ -1,5 +1,7 @@
 from datetime import timedelta
 
+from .base import DEBUG
+
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=5),
@@ -7,9 +9,18 @@ SIMPLE_JWT = {
 }
 
 REST_FRAMEWORK = {
+    "DEFAULT_RENDERER_CLASSES": ("rest_framework.renderers.JSONRenderer",),
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
 }
+
+
+if DEBUG:
+    # Allow browsable API only in dev mode
+    REST_FRAMEWORK["DEFAULT_RENDERER_CLASSES"] = (
+        "rest_framework.renderers.JSONRenderer",
+        "rest_framework.renderers.BrowsableAPIRenderer",
+    )
