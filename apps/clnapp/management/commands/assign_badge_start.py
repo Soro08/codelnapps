dae3b9f0de60321d0225c42cf2631d4182da652a
Ib soro <soro4827@gmail.com>
2023-11-05 23:28:38 +0000

    fix: flake8

--- apps/clnapp/management/commands/assign_badge_start.py
+++ apps/clnapp/management/commands/assign_badge_start.py
@@ -21,4 +21,4 @@ class Command(BaseCommand):
         for m3d in model3ds:
             add_user_bage(m3d.author, BADGE_START)
 
-        self.stdout.write(self.style.SUCCESS(f"badge distribués."))
+        self.stdout.write(self.style.SUCCESS("badge distribués."))
