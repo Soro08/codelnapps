b88cd453b418be1cfb7bf3a93191cfc49e33bcb1
Ib soro <soro4827@gmail.com>
2023-11-05 20:32:31 +0000

    update code

---
+++ apps/clnapp/tests/views/test_views.py
@@ -0,0 +1,41 @@
+from django.contrib.auth.models import User
+from django.urls import reverse
+from rest_framework import status
+from clnapp.tests import TestCase
+from clnapp.models import Model3d, UserBadge
+from clnapp.models.constants import BADGE_COLLECTOR
+
+
+class RegisterUserTestCase(TestCase):
+    def test_create_user(self):
+        nb_user_before_add = User.objects.count()
+        data = {
+            "username": "soro4",
+            "email": "kone@gmail.com",
+            "first_name": "KONE",
+            "last_name": "BEMA",
+            "password": "AzertyKlm",
+            "password2": "AzertyKlm",
+        }
+
+        response = self.client.post(reverse("users-list"), data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        nb_user_after_add = User.objects.count()
+        self.assertEqual(nb_user_before_add + 1, nb_user_after_add)
+
+    def test_create_user_invalid_data(self):
+        nb_user_before_add = User.objects.count()
+        data = {
+            "email": "kone@gmail.com",
+            "first_name": "KONE",
+            "last_name": "BEMA",
+            "password": "AzertyKlm",
+            "password2": "AzertyKlm",
+        }
+
+        response = self.client.post(reverse("users-list"), data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        nb_user_after_add = User.objects.count()
+        self.assertNotEqual(nb_user_before_add + 1, nb_user_after_add)
