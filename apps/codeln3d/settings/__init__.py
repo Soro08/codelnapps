7ef1f93e06eaeaec5b8e5475d847a82b12308980
Ib soro <soro4827@gmail.com>
2023-11-21 12:19:10 +0000

    fix: rm celery to docker-compose file

--- apps/codeln3d/settings/__init__.py
+++ apps/codeln3d/settings/__init__.py
@@ -1,7 +1,8 @@
 from .app import *
 from .base import *
 from .drf import *
-from .celery import *
+
+# from .celery import *
 from .language import *
 from .static import *
 from .template import *
